2bc0efd1032d94f86a4c457444d9bf7a4f1ce156
Milosz214234t4 <78423365+Milosz-N@users.noreply.github.com>
2023-01-28 00:30:05 +0100

    zmiana id i pozycji v1

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,38 @@
 import Settings from "./Settings";
 import "../components/scss/game.scss";
 import React, { useState, useEffect } from "react";
-function Game({ image, startGame }) {
+function Game({image, startGame }) {
   const [card, setCard] = useState([]);
-  const [counter, setCounter] = useState(0);
-  const [clicked, setClicked] = useState([]);
+  const [cardtest, setCardTest] = useState([]);
+  var arr = [];
+
+  function handleSubmit(e) {
+    const button = document.querySelectorAll(".part");
+    if (arr.length == 0 || e.target.id != arr[arr.length - 1]) {
+      arr.push(e.target.id);
+      if (arr.length % 2 === 0) {
+        for (Element of button) {
+          if (Element.id == arr[arr.length - 1]) {
+            Element.setAttribute("id", `${arr[arr.length - 2]}`);
+            let x = Number.parseInt(arr[arr.length - 2][0]);
+            let y = Number.parseInt(arr[arr.length - 2][2]);
+            Element.style.backgroundPositionY = `${720 - y * 144}px`;
+            Element.style.backgroundPositionX = `${1280 - x * 256}px`;
+          } else if (Element.id == arr[arr.length - 2]) {
+            // console.log("pierwszy klikniety element")
+            Element.setAttribute("id", `${arr[arr.length - 1]}`);
+            // console.log(Element.style.backgroundPositionX);
+            let x = Number.parseInt(arr[arr.length - 1][0]);
+            let y = Number.parseInt(arr[arr.length - 1][2]);
+            Element.style.backgroundPositionY = `${720 - y * 144}px`;
+            Element.style.backgroundPositionX = `${1280 - x * 256}px`;
+          }
+        }
+      }
+    }
+   
+  }
+
   useEffect(() => {
     let arr = [];
     let arrboard = [];
@@ -20,76 +48,39 @@ function Game({ image, startGame }) {
       for (let x = 0; x < 25; x++) {
         var random = Math.floor(Math.random() * arr.length);
         let randomNum = arr[random];
+        // console.log(asd);
         arr = arr.filter(function (e) {
           return e !== arr[random];
         });
 
-        setCard((prevState) => [
+        setCardTest((prevState) => [
           ...prevState,
-          React.createElement(
-            "button",
-            {
-              style: {
-                 backgroundImage: `url(${image})`,
-                  backgroundPositionX: `${1280 - randomNum[0] * 256}px`,
-                  backgroundPositionY: `${720 - randomNum[1] * 144}px`,
-                width: `256px`,
-                height: `144px`,
-              },
-              className: `board`,
-              key: arrboard[x],
-              id: randomNum,
-
-              onClick: handleSubmit,
+          React.createElement("button", {
+            style: {
+              backgroundImage: `url(${image})`,
+              backgroundPositionX: `${1280 - randomNum[0] * 256}px`,
+              backgroundPositionY: `${720 - randomNum[1] * 144}px`,
+              width: `256px`,
+              height: `144px`,
             },
-            [
-              React.createElement("div", {
-                style: {
-                 
-                  width: `256px`,
-                  height: `144px`,
-                },
-                className: `part`,
-                key: arrboard[x],
-                id: arrboard[x],
-
-              }),
-            ]
-          ),
+            className: `part`,
+            id: randomNum,
+            key: arrboard[x],
+            name: arrboard[x],
+            onClick: handleSubmit,
+          }),
         ]);
       }
-    }
-    function handleSubmit(e) {
-      setCounter((prevState) => prevState + 1);
-      clicked.push(e.target.parentNode.id);
-      e.target.parentElement.disabled = true;
-      console.log(e.target.parentElement.disabled);
-      console.log(e);
-       if(clicked.length % 2 == 0) {
-        console.log("sprawdzam");
-        console.log(clicked);
-        const firstChecked = document.querySelectorAll(
-          `.board[disabled='']`
-        );
      
-        console.log(firstChecked[0]);
-       
-     
-          firstChecked[0].setAttribute("id", `${clicked[clicked.length-2]}`);
-          firstChecked[1].setAttribute("id", `${clicked[clicked.length-1]}`);
-      
-          for(Element of firstChecked){
-              Element.disabled = false
-          }
-      }
-    console.log(clicked)
     }
   }, [startGame]);
+
   return (
-    <>
-      <div className="cardsContainer">{card}</div>
-      <h2>{counter % 2}</h2>
-    </>
+    startGame === true && (
+      <>
+        <div className="cardsContainer">{cardtest}</div>
+      </>
+    )
   );
 }
 
